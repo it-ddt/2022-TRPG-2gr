e6f9ec6bd3f8cdf5747c38b9ce54f7ba6dbff4df
it-ddt <45958272+it-ddt@users.noreply.github.com>
2022-11-14 14:05:35 +0300

    Add files via upload

--- casino.py
+++ casino.py
@@ -1,16 +1,14 @@
 import os
-import hero_engine
 
 
 def show(player):
     money = player[5]
-    potions = player[6]
 
     while True:
 
         # очищаем экран и показываем текст лавки
         os.system("cls")
-        print(f"{player[0]} приехал в лавку алхимика")
+        print(f"{player[0]} приехал в трактир. Трактирщик Харитон предлагает сыграть в кости на деньги.")
 
         # показываем персонажа
         print("имя:", player[0])
@@ -23,8 +21,8 @@ def show(player):
         print("")
 
         # показываем варианты в лавке
-        print("1 - купить зелье за 10 монет")
-        print("2 - уехать обратно к камню")
+        print("1 - Сыграть в кости")
+        print("2 - Уехать обратно к камню")
         answer = input("\nВведите номер варианта и нажмите ENTER: ")
         if answer == "1":
             os.system("cls")
@@ -36,4 +34,4 @@ def show(player):
                 print("Недостаточно монет!")
             input("\nНажмите ENTER чтобы продолжить")
         elif answer == "2":
-            return (player[0], player[1], player[2], player[3], player[4], money, potions)
+            return (player[0], player[1], player[2], player[3], player[4], money, player[6])
--- game.py
+++ game.py
@@ -1,25 +1,22 @@
 import os
 import shop
+import hero_engine
+
 
 def start_game():
     os.system("cls")
     print("Игра началась")
 
     # создаем игрока - КОРТЕЖ
-    # [0] имя, [1] здоровье, [2] деньги, [3] зелья
-    player = ("Вася Питонов", 100, 50, 0)
+    player = hero_engine.make_hero(name="Вася Питонов")
+
 
     # начался главный цикл игры
     while True:
         os.system("cls")
 
         # показываем персонажа
-        print("Персонаж: ")
-        print("имя:", player[0])
-        print("здоровье:", player[1])
-        print("деньги:", player[2])
-        print("зелья:", player[3])
-        print("")
+        hero_engine.show_hero(player)
 
         # показываем варианты
         print("1 - Поехать в лавку алхимика")
@@ -29,5 +26,8 @@ def start_game():
         answer = input("\nВведите номер варианта и нажмите ENTER: ")
         if answer == "1":
             player = shop.show(player)
+        elif answer == "2":
+            print("едем в казино")
+
 
 start_game()
